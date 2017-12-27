103953135d97a895cfdc9c268cce4d04676808d4
victor <vili@nvidia.com>
2017-12-27 18:56:14 +0800

    update the command

--- examples/protein/run_cmd.sh
+++ examples/protein/run_cmd.sh
@@ -3,10 +3,10 @@ gmx pdb2gmx -ignh -ff amber99sb-ildn -f 1OMB_fws.pdb -o fws.gro -p fws.top -wate
 gmx editconf -f fws.gro -o fws-PBC.gro -bt dodecahedron -d 1.2
 gmx grompp -f em-vac-pme.mdp -c fws-PBC.gro -p fws.top -o em-vac.tpr -maxwarn 99999999
 gmx mdrun -v -deffnm em-vac
-gmx solvate -cp em-vac.gro -cs spc216.gro -p fws.top -o fws-b4ion.gro
-gmx grompp -f em-sol-pme.mdp -c fws-b4ion.gro -p fws.top -o ion.tpr -maxwarn 99999999
-gmx genion -s ion.tpr -o fws-b4em.gro -neutral -conc 0.15 -p fws.top
-gmx grompp -f em-sol-pme.mdp -c fws-b4em.gro -p fws.top -o em-sol.tpr
-gmx mdrun -v -deffnm em-sol
-gmx grompp -f nvt-pr-md.mdp -c em-sol.gro -p fws.top -o nvt-pr.tpr
-gmx mdrun -v -deffnm nvt-pr
+#gmx solvate -cp em-vac.gro -cs spc216.gro -p fws.top -o fws-b4ion.gro
+#gmx grompp -f em-sol-pme.mdp -c fws-b4ion.gro -p fws.top -o ion.tpr -maxwarn 99999999
+#gmx genion -s ion.tpr -o fws-b4em.gro -neutral -conc 0.15 -p fws.top
+#gmx grompp -f em-sol-pme.mdp -c fws-b4em.gro -p fws.top -o em-sol.tpr
+#gmx mdrun -v -deffnm em-sol
+#gmx grompp -f nvt-pr-md.mdp -c em-sol.gro -p fws.top -o nvt-pr.tpr
+#gmx mdrun -v -deffnm nvt-pr
